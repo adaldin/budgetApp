f21fb22c7288d1c03f9b393dd4517613facfe8f8
Hiena <aledaldin@gmail.com>
2022-05-17 17:53:23 +0200

    BrowserRouter created + Header-Footer

--- budget-app/src/App.js
+++ budget-app/src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import React from "react";
 
 function App() {
-  return <div className="App">{/* routing */}</div>;
+  return (
+    <BrowserRouter>
+      <Header></Header>
+      <Switch>
+        <Route path="/welcome">
+          <Welcome></Welcome>
+        </Route>
+        <Route path="/budget">
+          <Budget></Budget>
+        </Route>
+      </Switch>
+
+      <Footer></Footer>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ budget-app/src/components/header/Header.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+function Header() {
+  return (
+    <React.Fragment>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/welcome">Home</Link>
+            </li>
+            <li>
+              <Link to="/budget">Get Your Budget</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </React.Fragment>
+  );
+}
+
+export default Header;
