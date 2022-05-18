bda6bae6eee2a50a713a0b3f619e32de3f38014a
Hiena <aledaldin@gmail.com>
2022-05-18 10:28:14 +0200

    SideBar amb Bootstrap

--- budget-app/src/components/sideBar/SideBar.js
+++ budget-app/src/components/sideBar/SideBar.js
@@ -1,21 +1,36 @@
-import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 function SideBar() {
   return (
-    <React.Fragment>
-      <label htmlFor="searcherBar"></label>
-      <input
-        type="text"
-        id="searcherBar"
-        placeholder="Busca tus presupuestos"
-        // checked={formData.}
-      />
-      <button>A-Z</button>
-      <button>01/11/2022</button>
-      <button>RESTART</button>
-      <h6>total</h6>
-      <p>300€</p>
-    </React.Fragment>
+    <Container fluid>
+      <Row className="my-2">
+        <label htmlFor="searcherBar"></label>
+        <input
+          type="text"
+          id="searcherBar"
+          placeholder="Busca tus presupuestos"
+          // checked={formData.}
+        />
+      </Row>
+      <Row className="my-2 d-flex w-100 g-2">
+        <Col sm={12} md={4}>
+          <Button variant="outline-light">A-Z</Button>
+        </Col>
+        <Col sm={12} md={4}>
+          <Button variant="outline-light">11/2022</Button>
+        </Col>
+        <Col sm={12} md={4}>
+          <Button variant="outline-light">RESTART</Button>
+        </Col>
+      </Row>
+      <Row className="my-2">
+        <h6>total</h6>
+        <p>300€</p>
+      </Row>
+    </Container>
   );
 }
 
