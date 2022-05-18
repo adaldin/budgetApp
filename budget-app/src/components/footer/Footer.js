f1e8dabfd009398b46958928fa8a7a72ba30601c
Hiena <aledaldin@gmail.com>
2022-05-18 09:53:33 +0200

    Footer amb Bootstrap

--- budget-app/src/components/footer/Footer.js
+++ budget-app/src/components/footer/Footer.js
@@ -1,8 +1,19 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Navbar from "react-bootstrap/Navbar";
+
 function Footer() {
   return (
-    <footer>
-      <p>footer</p>
-    </footer>
+    <Navbar bg="light" variant="light">
+      <Container fluid>
+        <Row className="w-100">
+          <Col sm={12} className="text-center">
+            <small>© X-Solutions</small>
+          </Col>
+        </Row>
+      </Container>
+    </Navbar>
   );
 }
 
--- budget-app/src/components/header/Header.js
+++ budget-app/src/components/header/Header.js
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
