7bc9ce41f4925044f8f9b6e78d590ba23f33d337
Hiena <aledaldin@gmail.com>
2022-05-18 10:03:04 +0200

    Budget page amb Bootstrap

--- budget-app/src/pages/budget.js
+++ budget-app/src/pages/budget.js
@@ -1,15 +1,21 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import BudgetForm from "../components/budgetForm/BudgetForm";
 import SideBar from "../components/sideBar/SideBar";
 
 function Budget() {
   return (
-    <Container fluid>
-      <main>
-        <BudgetForm></BudgetForm>
-        <SideBar></SideBar>
-      </main>
+    <Container fluid className="mt-5 bg-secondary text-light p-5">
+      <Row>
+        <Col sm={12} md={8}>
+          <BudgetForm></BudgetForm>
+        </Col>
+        <Col sm={12} md={4}>
+          <SideBar></SideBar>
+        </Col>
+      </Row>
     </Container>
   );
 }
--- budget-app/src/pages/welcome.js
+++ budget-app/src/pages/welcome.js
@@ -2,24 +2,31 @@ import React from "react";
 import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
 
 function Welcome() {
   return (
     <Container fluid className="mt-5 bg-secondary text-light p-5">
       <Row>
-        <h1>Welcome!</h1>
+        <Col sm={12}>
+          <h1>Welcome!</h1>
+        </Col>
       </Row>
       <Row>
-        <p>
-          is simply dummy text of the printing and typesetting industry. Lorem
-          Ipsum has been the industry's standard dummy text ever since the
-          1500s, when an unknown printer took a galley of type and scrambled it
-          to make a type specimen book. It has survived not only five centuries,
-          but also the leap into electronic typesetting, remaining essentially
-          unchanged.{" "}
-        </p>
+        <Col sm={12} md={6}>
+          <p>
+            is simply dummy text of the printing and typesetting industry. Lorem
+            Ipsum has been the industry's standard dummy text ever since the
+            1500s, when an unknown printer took a galley of type and scrambled
+            it to make a type specimen book. It has survived not only five
+            centuries, but also the leap into electronic typesetting, remaining
+            essentially unchanged.{" "}
+          </p>
+        </Col>
+        <Col sm={12} md={6}>
+          <Button variant="outline-light">Get your Budget</Button>
+        </Col>
       </Row>
-      <Button variant="outline-light">Get your Budget</Button>
     </Container>
   );
 }
